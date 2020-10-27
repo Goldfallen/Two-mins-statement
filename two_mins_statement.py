a2aa3a2633f451c2fbd500594a7713b9d10c46d8
Goldfallen <62795250+Goldfallen@users.noreply.github.com>
2020-10-27 21:39:39 +0800

    Second version of  personal financial statement
    Improved feature:-
    -Utilized the csv file exported by Spendee application
    -Able to upload the file from my folder directly
    -Able to export as excel to my folder directly

--- two_mins_statement.py
+++ two_mins_statement.py
@@ -1,260 +1,295 @@
+#print("Target 2 mins statement. Goal short 10% of v1 code length")
 import csv
 import itertools
-#import pandas as pd
-#import tkinter as tk
-#from tkinter import filedialog
+import os
+import pandas as pd
 
 
-with open('expenses.csv', newline='') as f:
+#setting my path so that i can export.xlsx file to my desired folders
+os.chdir("C:\\Users\\NkyGogo\\Desktop\\Development\\Re_Engineer\\2. Personal Project\\1.One code a day\\Sorcery Research\\Two Minutes Statements")
+
+#direct to the folder that contain my csv file
+with open('C:\\Users\\NkyGogo\\Desktop\\Development\\Re_Engineer\\2. Personal Project\\1.One code a day\\Sorcery Research\\Two Minutes Statements\\expenses.csv', newline='') as f:
   reader = csv.reader(f)
   data = list(reader)
+  
 remove_title = data.pop(0)
 
 raw_string = []
 
-for item in data:
-  for info in item:
-    raw_string.append(info.split(';'))
+
+def export_copy(statement):
+    confirmation = input("Boss, do you want to export the statement to excel? Enter y/n: ")
+    if confirmation == "y":
+        statement.to_excel("First_export.xlsx")
+ 
+def money_come_come():
+  for item in data:
+    for info in item:
+      raw_string.append(info.split(';'))
+  record_date =  []
+  record_user = []
+  record_type =  []
+  record_categories = []
+  expense_amount = []
+  record_currency = []
+  expense_type = []
+  record_first_name =[ ]
+  record_sub_name = []
+
+  for daily_record in raw_string:
+    for i in range(len(daily_record)):
+      if i == 0:
+        record_date.append(daily_record[i])
+      elif i == 1:
+        record_user.append(daily_record[i])
+      elif i == 2:
+        record_type.append(daily_record[i])
+      elif i == 3:
+        record_categories.append(daily_record[i])
+      elif i == 4:
+        expense_amount.append(daily_record[i].strip('"').strip('-'))
+      elif i == 5:
+        record_currency.append(daily_record[i])
+      elif i == 6:
+        expense_type.append(daily_record[i].lower().strip('"').split(' '))
+      elif i == 7:
+        record_first_name.append(daily_record[i])
+      elif i == 8:
+        record_sub_name.append(daily_record[i])
 
 
-#print(raw_string)
-record_date = []
-wallet_name = []
-expense_type = []
-set_category = []
-amount = []
-currency = []
-notes = []
-first_name = []
-last_name = []
+  expense_reference = []
+  expense_type_v1 = list(itertools.chain(*expense_type))
 
 
-for block in raw_string:
-  for idx in range(len(block)):
-    if idx == 0:
-      record_date.append(block[idx])
-    elif idx == 1:
-      wallet_name.append(block[idx])
-    elif idx == 2:
-      expense_type.append(block[idx])
-    elif idx == 3:
-      set_category.append(block[idx])
-    elif idx == 4:
-      amount.append(block[idx])
-    elif idx == 5:
-      currency.append(block[idx])
-    elif idx == 6:
-      notes.append(block[idx])
-    elif idx == 7:
-      first_name.append(block[idx])
-    elif idx == 8:
-      last_name.append(block[idx])
+  for item in expense_type_v1:
+    if item[:3] !=  "#rm" and item:
+      expense_reference.append(item.strip('#'))
+
+  
+  fixed_expense_amount = 0
+  variable_expense_amount = 0
+  discretionary_expense_amount= 0
 
 
-#raw data list
-record_date_v2 = []
-set_category_v2 = []
-amount_v2 = []
-amount_v3 = []
-notes_v2 = []
-notes_v3 = []
-payment_mode = []
+  #fixed expense list
+  parent_contribution = ['parent_contribution']
+  rental = []
+  mobile = ['mobile']
+  insurance = ['insurance']
+
+  parent_contribution_amount = 0
+  rental_amount = 0
+  mobile_amount = 0
+  insurance_amount = 0
 
 
-#tidy up the data and input into raw data list
+  #variable expense list
+  food = ['takeaway', 'dinningout', 'foodbank']
+  petrol =  ['petrol']
+  vehicle_service = []
+  tot = []
+  vehicle_accesories = []
+  parking = []
 
-def strip_anymark(old_list,new_list,mark):
-  for list_item in old_list:
-    new_list.append(list_item.strip(mark))
-
-strip_anymark(record_date, record_date_v2,'"')
-strip_anymark(set_category, set_category_v2,'"')
-strip_anymark(amount, amount_v2,'"')
-strip_anymark(notes, notes_v2 ,'"')
-strip_anymark(amount_v2, amount_v3 ,'-')
-
-
-for i in notes_v2:
-  draft_note = i.strip(' ').split(' ')
-  element_1 = draft_note[0].strip('#').lower()
-  element_2 = draft_note[-1].strip('#').lower()
-  if element_2 == 'rm_bank' or element_2=='rm_cash':
-    notes_v3.append(element_1)
-    payment_mode.append(element_2)
-  else:
-    notes_v3.append(element_2)
-    payment_mode.append(element_1)
+  food_amount = 0
+  petrol_amount = 0
+  vehicle_service_amount = 0
+  tot_amount = 0
+  vehicle_accesories_amount = 0
+  parking_amount = 0
 
 
+  #discretionary_expense list
+  education = ['learning']
+  subscription = ['subscription']
+  shopping = []
+  medical = []
+  personal_care = ['personalcare']
+  sport = ['fitness']
+  loan_repayment = []
+  penalty = []
+  car_insurance = []
+  other_transports = []
+  tax = []
+  working_miscellaneous = ['work']
+  charity = []
 
-#categorizing the expense here
-fixed_expense_list = []
-fixed_expense_amount = 0
-variable_expense_list = []
-variable_expense_amount = 0
-discretionary_expense_list =[]
-discretionary_expense_amount = 0
+  education_amount = 0
+  subscription_amount = 0
+  shopping_amount = 0
+  medical_amount = 0
+  personal_care_amount = 0
+  sport_amount = 0
+  loan_repayment_amount = 0
+  penalty_amount = 0
+  car_insurance_amount = 0
+  other_transports_amount = 0
+  tax_amount = 0
+  working_miscellaneous_amount = 0
+  charity_amount = 0
 
-#check
-#add
-
-#subvintentory
-parent_contribution = ['parent_contribution']
-rental = []
-mobile =  ['mobile']
-insurance = ['insurance']
-
-fixed_expense_list.append(parent_contribution)
-fixed_expense_list.append(rental)
-fixed_expense_list.append(mobile)
-fixed_expense_list.append(insurance)
-
-
-food = ['dinningout', 'foodbank', 'takeaway']
-petrol = ['petrol']
-vehicle_service = []
-tot = []
-vehicle_accesories = []
-parking = []
-
-variable_expense_list.append(food)
-variable_expense_list.append(petrol)
-variable_expense_list.append(vehicle_service)
-variable_expense_list.append(tot)
-variable_expense_list.append(vehicle_accesories)
-variable_expense_list.append(parking)
-
-
-education = ['learning']
-subscription = ['subscription']
-shopping = []
-medical = []
-personal_care = ['personalcare']
-sport = ['fitness']
-loan_repayment = []
-penalty = []
-car_insurance = []
-other_transports = []
-tax = []
-working_miscellaneous = ['work']
-charity = [] 
+    #fixed expense start count here
+  for i in range(len(expense_reference)):
+    if expense_reference[i] in parent_contribution:
+      parent_contribution_amount += float(expense_amount[i])
+      fixed_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in rental:
+      rental_amount += float(expense_amount[i])
+      fixed_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in mobile:
+      mobile_amount += float(expense_amount[i])
+      fixed_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in insurance:
+      insurance_amount += float(expense_amount[i])
+      fixed_expense_amount += float(expense_amount[i])
 
 
-discretionary_expense_list.append(education)
-discretionary_expense_list.append(subscription)
-discretionary_expense_list.append(shopping)
-discretionary_expense_list.append(medical)
-discretionary_expense_list.append(personal_care)
-discretionary_expense_list.append(sport)
-discretionary_expense_list.append(loan_repayment)
-discretionary_expense_list.append(penalty)
-discretionary_expense_list.append(car_insurance)
-discretionary_expense_list.append(other_transports)
-discretionary_expense_list.append(tax)
-discretionary_expense_list.append(working_miscellaneous)
-discretionary_expense_list.append(charity)
+    #variable expense start count here
+    elif expense_reference[i] in food:
+      food_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in petrol:
+      petrol_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in vehicle_service:
+      vehicle_service_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in tot:
+      tot_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in vehicle_accesories:
+      vehicle_accesories_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in parking:
+      parking_amount += float(expense_amount[i])
+      variable_expense_amount += float(expense_amount[i])
 
 
-#open source code for flatenning list
-def oneDArray(x):
-    return list(itertools.chain(*x))
-
-
-
-#Just use it to check notes_v3 vs categorizing, if there is miss out, manual key into the categories
-def record_once_check_exist(note_list, cat_1 = fixed_expense_list , cat_2 = variable_expense_list, cat_3= discretionary_expense_list):
-  ind_list = note_list.copy()
-  ind_list_v1 = []
-  for item_1 in ind_list:
-    #use linear search to eliminate the duplicate item
-    if item_1 not in ind_list_v1:
-      ind_list_v1.append(item_1)
-
-  uncategorized_item = []
-
-  for item_2 in ind_list_v1:
-
-    cat_a = oneDArray(cat_1)
-    cat_b = oneDArray(cat_2)
-    cat_c = oneDArray(cat_3)
-    if (not item_2 in cat_a) and (not item_2 in cat_b) and (not item_2 in cat_c):
-      uncategorized_item.append(item_2)
-  if uncategorized_item:
-    print("LOL XD There are uncategorized items, which are:")
-    for non_found in uncategorized_item:
-      print("- " + non_found)
-
-
-#record_once_check_exist(notes_v3)
-
-
-#calculate
-fixed_expense_list_v1 = oneDArray(fixed_expense_list)
-variable_expense_list_v1 = oneDArray(variable_expense_list)
-discretionary_expense_list_v1 = oneDArray(discretionary_expense_list)
-
-
-for i in range(len(notes_v3)):
-  temp_number = amount_v3[i]
-  if notes_v3[i] in fixed_expense_list_v1:
-    fixed_expense_amount += float(temp_number)
-  elif notes_v3[i] in variable_expense_list_v1:
-    variable_expense_amount += float(temp_number)
-  elif notes_v3[i] in discretionary_expense_list_v1:
-    discretionary_expense_amount += float(temp_number)
-  else:
-    record_once_check_exist(notes_v3)
-
-
-total_month_expense = float(fixed_expense_amount) + float(variable_expense_amount) + float(discretionary_expense_amount)
-
-
-#print statement
-
-
-income = input("Huhuhu...How much you earn this month @@ \n")
-net_loss_or_surplus = float(income) - float(total_month_expense)
-def statement_presentation():
-  if float(income) > 0:
-    print ("Fixed Expense: " + '%.2f' % fixed_expense_amount)
-    print ("Variable Expense: " + '%.2f' % variable_expense_amount)
-    print ("Discretionary Expense: " + '%.2f' % discretionary_expense_amount)
-    print ("Total Expense: " + '%.2f' % total_month_expense )
-    print ("Net surplus/loss: " + '%.2f' % net_loss_or_surplus )
-
-statement_presentation()
-
-
-statement = {'Category': ['Income','Fixed Expense','Variable Expense','Discretionary Expense', 'Total Expense', 'Net surplus/loss'],
-        'Amount': ['%.2f' % float(income) ,'%.2f' % float(fixed_expense_amount),'%.2f' % float(variable_expense_amount),'%.2f' % float(discretionary_expense_amount), '%.2f' % float(total_month_expense), '%.2f' % float(net_loss_or_surplus)]
-        }
-
-
-'''
-#refer to https://datatofish.com/export-dataframe-to-excel/
-
-
-df = pd.DataFrame(statement, columns = ['Category', 'Amount'])
+    #discretionary expense list
+    elif expense_reference[i] in education:
+      education_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in subscription:
+      subscription_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in shopping:
+      shopping_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in medical:
+      medical_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in personal_care:
+      personal_care_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in sport:
+      sport_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in loan_repayment:
+      loan_repayment_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in penalty:
+      penalty_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in car_insurance:
+      car_insurance_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in other_transports:
+      other_transports_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in tax:
+      tax_amount +=float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in working_miscellaneous:
+      working_miscellaneous_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    elif expense_reference[i] in charity:
+      charity_amount += float(expense_amount[i])
+      discretionary_expense_amount += float(expense_amount[i])
+    else:
+      print("Emmm...can\'t find {a} in recorded categories, just adding it now LOL".format(a= expense_amount[i]) + "\n")
 
 
 
-root= tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue2', relief = 'raised')
-canvas1.pack()
-
-def exportExcel ():
-    global df
-
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.xlsx')
-    df.to_excel (export_file_path, index = False, header=True)
-
-saveAsButtonExcel = tk.Button(text='Export Excel', command=exportExcel, bg='green', fg='white', font=('helvetica', 12, 'bold'))
-canvas1.create_window(150, 150, window=saveAsButtonExcel)
-
-root.mainloop()
-'''
 
 
+  print('Parent Contribution: ' + '%.2f' % parent_contribution_amount)
+  print('Rental: ' + '%.2f' % rental_amount)
+  print('Mobile: ' + '%.2f' % mobile_amount)
+  print('Insurance: ' + '%.2f' % insurance_amount)
+  print('Total Fixed Expense: ' + '%.2f' % fixed_expense_amount + '\n' )
 
-#code income and calculate the surplus too, as well as other items in expense, and financial ratio
+  print('Food: ' + '%.2f' %food_amount)
+  print('Petrol: ' + '%.2f' %petrol_amount)
+  print('Vehicle Service: ' + '%.2f' %vehicle_service_amount)
+  print('Tot: ' + '%.2f' %tot_amount)
+  print('Vehicle Accessories: ' + '%.2f' %vehicle_accesories_amount)
+  print('Parking: ' + '%.2f' %parking_amount)
+  print('Total Variable Expense: ' + '%.2f' %variable_expense_amount + '\n')
+
+  print('Education: ' + '%.2f' %education_amount)
+  print('Subscription: ' + '%.2f' %subscription_amount)
+  print('Shopping: ' + '%.2f' %shopping_amount)
+  print('Medical: ' + '%.2f' %medical_amount)
+  print('Personal Care: ' + '%.2f' %personal_care_amount)
+  print('Sport: ' + '%.2f' %sport_amount)
+  print('Loan Repayment: ' + '%.2f' %loan_repayment_amount)
+  print('Penalty: ' + '%.2f' %penalty_amount)
+  print('Car Insurance: ' + '%.2f' %car_insurance_amount)
+  print('Other Transports: ' + '%.2f' %other_transports_amount)
+  print('Tax: ' + '%.2f' %tax_amount)
+  print('Work Miscellaneous: ' +'%.2f' % working_miscellaneous_amount)
+  print('Charity: ' + '%.2f' %charity_amount)
+  print('Total Discretionary Expense: ' + '%.2f' %discretionary_expense_amount + '\n')
+
+
+  total_expense = float(fixed_expense_amount) + float(variable_expense_amount) + float(discretionary_expense_amount)
+
+  print('Total Monthly Expense: ' + str(total_expense) + "\n")
+  
+  
+  export_frame = pd.DataFrame({"Items":['Parent Contribution','Rental',
+                                        'Mobile','Insurance',
+                                        'Total Fixed Expense',
+                                        'Food','Petrol','Vehicle Service',
+                                        'Tot','Vehicle Accessories','Parking',
+                                        'Total Variable Expense','Education',
+                                        'Subscription','Shopping','Medical',
+                                        'Personal Care','Sport',
+                                        'Loan Repayment','Penalty',
+                                        'Car Insurance','Other Transports',
+                                        'Tax','Work Miscellaneous','Charity',
+                                        'Total Discretionary Expense', 
+                                        'Total Monthly Expense'], 
+                               "Amount":[parent_contribution_amount, 
+                                         rental_amount, mobile_amount, 
+                                         insurance_amount, fixed_expense_amount,
+                                         food_amount, petrol_amount,
+                                         vehicle_service_amount,tot_amount,
+                                         vehicle_accesories_amount, 
+                                         parking_amount, 
+                                         variable_expense_amount, 
+                                         education_amount, subscription_amount,
+                                         shopping_amount, medical_amount, 
+                                         personal_care_amount, sport_amount,
+                                         loan_repayment_amount, penalty_amount,
+                                         car_insurance_amount, 
+                                         other_transports_amount, tax_amount, 
+                                         working_miscellaneous_amount, 
+                                         charity_amount, 
+                                         discretionary_expense_amount, 
+                                         total_expense]})
+  export_copy(export_frame)
+  
+
+
+money_come_come()
+
+
+    
+    
+
+
+
+
